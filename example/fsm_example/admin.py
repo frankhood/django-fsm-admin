27e7f4dde559319403f52776d0061d77ba96916f
Adriano <a.palumbo@frankhood.it>
2024-09-06 14:16:03 +0200

    update ugettext to gettext_lazy

--- example/fsm_example/admin.py
+++ example/fsm_example/admin.py
@@ -1,22 +1,17 @@
 from django.contrib import admin
+from fsm_example.models import PublishableModel
 
 from fsm_admin.mixins import FSMTransitionMixin
-from fsm_example.models import PublishableModel
 
 
 # Example use of FSMTransitionMixin (order is important!)
+@admin.register(PublishableModel)
 class PublishableModelAdmin(FSMTransitionMixin, admin.ModelAdmin):
     list_display = (
-        'name',
-        'display_from',
-        'display_until',
-        'state',
+        "name",
+        "display_from",
+        "display_until",
+        "state",
     )
-    list_filter = (
-        'state',
-    )
-    readonly_fields = (
-        'state',
-    )
-
-admin.site.register(PublishableModel, PublishableModelAdmin)
+    list_filter = ("state",)
+    readonly_fields = ("state",)
